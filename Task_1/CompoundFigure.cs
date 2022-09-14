cc06d05959586a52117de100a7e10fbc1d2ff037
12den24@gmail.com <12den24@gmail.com>
2022-09-14 21:58:55 +0300

    interface

--- Task_1/CompoundFigure.cs
+++ Task_1/CompoundFigure.cs
@@ -4,47 +4,57 @@ namespace Task_1
 {
     internal class CompoundFigure
     {
-        GeometricFigure[] geometricFigures;
+        //GeometricFigure[] geometricFigures;
+        IGeometricFigure[] figure;
 
-        public CompoundFigure(params GeometricFigure[] geometricFigures)
+        //public CompoundFigure(params GeometricFigure[] geometricFigures)
+        //{
+        //    this.geometricFigures = geometricFigures;
+        //}
+
+        public CompoundFigure(params IGeometricFigure[] geometricFigures)
         {
-            this.geometricFigures = geometricFigures;
+            figure = geometricFigures;
         }
 
         public double AreaCompaundFigure()
         {
             double area = 0;
-            foreach (var item in geometricFigures)
+            foreach (var item in figure)
             {
-                if (item is Triangle)
-                {
-                    area += (item as Triangle).Area();
-                }
-                else if (item is Square)
-                {
-                    area += (item as Square).Area();
-                }
-                else if (item is Rhomb)
-                {
-                    area += (item as Rhomb).Area();
-                }
-                else if (item is Rectangle)
-                {
-                    area += (item as Rectangle).Area();
-                }
-                else if (item is Parallelogramm)
-                {
-                    area += (item as Parallelogramm).Area();
-                }
-                else if (item is Ellipse)
-                {
-                    area += (item as Ellipse).Area();
-                }
-                else if (item is Circle)
-                {
-                    area += (item as Circle).Area();
-                }
+                area += item.Area();
             }
+            //foreach (var item in geometricFigures)
+            //{
+            //    if (item is Triangle)
+            //    {
+            //        area += (item as Triangle).Area();
+            //    }
+            //    else if (item is Square)
+            //    {
+            //        area += (item as Square).Area();
+            //    }
+            //    else if (item is Rhomb)
+            //    {
+            //        area += (item as Rhomb).Area();
+            //    }
+            //    else if (item is Rectangle)
+            //    {
+            //        area += (item as Rectangle).Area();
+            //    }
+            //    else if (item is Parallelogramm)
+            //    {
+            //        area += (item as Parallelogramm).Area();
+            //    }
+            //    else if (item is Ellipse)
+            //    {
+            //        area += (item as Ellipse).Area();
+            //    }
+            //    else if (item is Circle)
+            //    {
+            //        area += (item as Circle).Area();
+            //    }
+            //}
             return area;
         }
 
